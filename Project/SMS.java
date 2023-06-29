86dd59cfa6e4bf53175572db38d5146aa2ee4608
Matheus Thiago <matheustheus27@gmail.com>
2023-06-29 17:22:43 -0300

    Update SMS.java

--- Project/SMS.java
+++ Project/SMS.java
@@ -510,16 +510,4 @@ public class SMS {
         return rules;
     }
     //#endregion
-
-    //#region FUNCTIONS SYSTEM
-    public static void clearConsole() throws IOException {
-        final String os = System.getProperty("os.name");
-
-        if (os.contains("Windows")) {
-            Runtime.getRuntime().exec("cls");
-        } else {
-            Runtime.getRuntime().exec("clear");
-        }
-    }
-    //#endregion
 }
