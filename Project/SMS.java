fb516e8e7c991a9b3c90e97bb17153af8b657754
Matheus Thiago <matheustheus27@gmail.com>
2023-06-29 18:28:22 -0300

    Update SMS.java

--- Project/SMS.java
+++ Project/SMS.java
@@ -235,13 +235,14 @@ public class SMS {
     }
 
     private void listSweepstakes() {
-        System.out.println("\n----- SMS - Lista de Sorteios -----");
         System.out.println("--- Filtro de Listagem ---");
         System.out.println("1. Pendentes");
         System.out.println("2. Em Progresso");
         System.out.println("3. Finalizados");
         System.out.println("4. Cancelados");
         System.out.println("0. Todos");
+
+        System.out.println("\n----- SMS - Lista de Sorteios -----");
 
         switch(scanner.next()) {
             case "1":
